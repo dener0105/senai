2fd7120ed2c54ba2ab95fb79bf3f168edfdc3dcd
dener0105 <razerdenerazer@hotmail.com>
2026-06-16 23:36:04 +0000

    Jun 16, 2026, 8:36 PM

--- matriz/matriz1.c
+++ matriz/matriz1.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 
-#define L
-#define C
-int matriz [L][C]:
-int i, j;
+int main() {
+    int matriz[3][4];
+    int i, j;
 
-for (i = 0; i < NL; i++) {
-    for (i = 0; i < NL; i++) {
-
-        printf("Informe Matriz[%d][%d]: ", i+1, j+1);
-        scanf("%d", &matriz[i][j]);
+    // Leitura dos valores
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 4; j++) {
+            printf("Digite matriz[%d][%d]: ", i + 1, j + 1);
+            scanf("%d", &matriz[i][j]);
         }
-}
-for (i = 0; i < NL; i++) {
-for (j = 0; j < NC; j++)
-printf("%5d ", matriz[i][j]);
-printf("\n");
+    }
+
+    // Exibição da matriz
+    printf("\nMatriz:\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 4; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
