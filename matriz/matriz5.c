68e5195267b0b32060ddae0e03a46d87549df02d
dener0105 <razerdenerazer@hotmail.com>
2026-06-23 22:28:50 +0000

    Jun 23, 2026, 7:28 PM

--- matriz/matriz5.c
+++ matriz/matriz5.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    int matriz [6][4];
-    int
+    int matriz[6][4];
+    int pares = 0;
+
+    for (int i = 0; i < 6; i++) {
+        for (int j = 0; j < 4; j++) {
+            scanf("%d", &matriz[i][j]);
+
+            if (matriz[i][j] % 2 == 0) {
+                pares++;
+            }
+        }
+    }
+
+    printf("Quantidade de pares: %d\n", pares);
+
+    return 0;
 }
