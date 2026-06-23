b8340d4b433437752eb574d8bbca5ad777a1cdd7
dener0105 <razerdenerazer@hotmail.com>
2026-06-23 23:20:41 +0000

    Jun 23, 2026, 8:20 PM

--- matriz/matriz7.c
+++ matriz/matriz7.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 
 int main() {
-    int matriz [5][3];
+    int matriz[5][3];
+    int i, j, coluna;
 
 
+    for (i = 0; i < 5; i++) {
+        for (j = 0; j < 3; j++) {
+            printf("Digite o valor para [%d][%d]: ", i, j);
+            scanf("%d", &matriz[i][j]);
+        }
+    }
 
+ 
+    printf("Digite a coluna  (0 a 2): ");
+    scanf("%d", &coluna);
+
+
+    printf("\nValores da coluna %d:\n", coluna);
+    for (i = 0; i < 5; i++) {
+        printf("%d\n", matriz[i][coluna]);
+    }
+
+    return 0;
 }
