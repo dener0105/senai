21388018ee160691136c24cbc25971452aea18e8
dener0105 <razerdenerazer@hotmail.com>
2026-06-23 21:53:32 +0000

    Jun 23, 2026, 6:53 PM

--- matriz/matriz1.c
+++ matriz/matriz1.c
@@ -1,26 +1,2 @@
 #include <stdio.h>
 
-#define L 3
-#define C 4
-
-int main() {
-    int matriz[3][4];
-    int i, j;
-
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 4; j++) {
-            printf("Digite matriz[%d][%d]: ", i + 1, j + 1);
-            scanf("%d", &matriz[i][j]);
-        }
-    }
-
-    printf("\nMatriz:\n");
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 4; j++) {
-            printf("%d ", matriz[i][j]);
-        }
-        printf("\n");
-    }
-
-    return 0;
-}
