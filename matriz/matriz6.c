43662b685628e22a726d7bc2d2696840af409f6d
dener0105 <razerdenerazer@hotmail.com>
2026-06-23 23:22:51 +0000

    Jun 23, 2026, 8:22 PM

--- matriz/matriz6.c
+++ matriz/matriz6.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
-#define L 4
-#define C 5
+
 int main() {
-    int matriz [4][5];
-    int 
+    int matriz[4][5];
+    int i, j, linha;
 
+    for (i = 0; i < 4; i++) {
+        for (j = 0; j < 5; j++) {
+            printf("Digite o valor para [%d][%d]: ", i, j);
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+
+    printf("\nDigite a linha (0 a 3): ");
+    scanf("%d", &linha);
+
+    printf("\nLinha escolhida:\n");
+    for (j = 0; j < 5; j++) {
+        printf("%d ", matriz[linha][j]);
+    }
+
+    printf("\n");
+
+    return 0;
 }
-
-
-
-
-
