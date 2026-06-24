e13f3c1e66b3622fca8982742262dc5258617ec1
dener0105 <razerdenerazer@hotmail.com>
2026-06-24 00:20:23 +0000

    Jun 23, 2026, 9:20 PM

--- matriz/matriz8.c
+++ matriz/matriz8.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 
-   int main(){
-   int matriz[5][5];
-   int I,j, coluna;
+int main() {
+    int matriz[5][5] = {
+        {6, 5, 8, 1, 7},
+        {1, 3, 7, 7, 7},
+        {4, 7, 9, 9, 1},
+        {5, 2, 2, 2, 6},
+        {6, 9, 2, 1, 0}
+    };
 
-for(i = 0;)
+    printf("Diagonal:\n");
 
+    for (int linha = 0; linha < 5; linha++) {
+        for (int coluna = 0; coluna < 5; coluna++) {
+            if (linha == coluna) {
+                printf("%d\n", matriz[linha][coluna]);
+            }
+        }
+    }
+
+    return 0;
 }
