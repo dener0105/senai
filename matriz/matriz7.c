5d39c9c7da8a6accfbdd6d5345365f287e7c4297
dener0105 <razerdenerazer@hotmail.com>
2026-06-24 00:03:27 +0000

    Jun 23, 2026, 9:03 PM

--- matriz/matriz7.c
+++ matriz/matriz7.c
@@ -16,11 +16,15 @@ int main() {
     printf("Digite a coluna  (0 a 2): ");
     scanf("%d", &coluna);
 
+    if(coluna >=3){
+        printf("valor invalido");
+    }else{
 
     printf("\nValores da coluna %d:\n", coluna);
     for (i = 0; i < 5; i++) {
         printf("%d\n", matriz[i][coluna]);
     }
 
-    return 0;
+       }
+          return 0;
 }
