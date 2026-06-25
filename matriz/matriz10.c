5328a63a7b560c8b63b7463becd60e12d96d0090
dener0105 <razerdenerazer@hotmail.com>
2026-06-25 22:03:51 +0000

    Jun 25, 2026, 7:03 PM

--- matriz/matriz10.c
+++ matriz/matriz10.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main(){
+#define l 3
+#define c 5
 
-    
+int estoque [3][5];
+int i, j, total =0, totalgeral, opcao;
+
+void cadastro de estoque(){
+    for i = 0; i <i
+}
+
+
 }
