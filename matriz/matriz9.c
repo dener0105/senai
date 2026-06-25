55aa84f1a1302dc63befe8ac9e7d0706251b0af4
dener0105 <razerdenerazer@hotmail.com>
2026-06-25 21:40:34 +0000

    Jun 25, 2026, 6:40 PM

--- matriz/matriz9.c
+++ matriz/matriz9.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 
-    int main() {
-   
-        
-        pritnf
+int main() {
+    int matriz[5][5];
+    int i, j, soma = 0;
 
+
+    printf("Digite os elementos da matriz 5x5:\n");
+    for (i = 0; i < 5; i++) {
+        for (j = 0; j < 5; j++) {
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+
+    for (i = 0; i < 5; i++) {
+        for (j = 0; j < 5; j++) {
+            if (i > j) {
+                soma += matriz[i][j];
+            }
+        }
+    }
+
+  
+    printf("Soma abaixo da diagonal = %d\n", soma);
+
+    return 0;
 }
