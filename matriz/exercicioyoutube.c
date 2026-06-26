332dd554809a9157ae50246088f789eea71d1e59
dener0105 <razerdenerazer@hotmail.com>
2026-06-26 00:06:53 +0000

    Jun 25, 2026, 9:06 PM

--- matriz/exercicioyoutube.c
+++ matriz/exercicioyoutube.c
@@ -7,4 +7,30 @@ int main()
 
     switch (mes)
 }
-case 1: printf ("o %dº mes do calendario e janeiro",mes);
+ case 1: printf ("o %dº mes do calendario e janeiro",mes);
+
+ case 2: printf ("o %dº mes do calendario e janeiro",mes);
+
+ case 3: printf ("o %dº mes do calendario e janeiro",mes);
+
+ case 4: printf ("o %dº mes do calendario e janeiro",mes);
+
+ case 5: printf ("o %dº mes do calendario e janeiro",mes);
+
+ case 6: printf ("o %dº mes do calendario e janeiro",mes);
+
+ case 7: printf ("o %dº mes do calendario e janeiro",mes);
+
+ case 8: printf ("o %dº mes do calendario e janeiro",mes);
+
+ case 9: printf ("o %dº mes do calendario e janeiro",mes);
+
+ case 10: printf ("o %dº mes do calendario e janeiro",mes);
+
+ case 11: printf ("o %dº mes do calendario e janeiro",mes);
+
+ case 12: printf ("o %dº mes do calendario e janeiro",mes);
+
+
+
+
