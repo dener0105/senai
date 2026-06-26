ce19a1832faf1db69755b5f843a20b3fc7109782
dener0105 <razerdenerazer@hotmail.com>
2026-06-26 00:20:50 +0000

    Jun 25, 2026, 9:20 PM

--- matriz/exercicioyoutube.c
+++ matriz/exercicioyoutube.c
@@ -6,33 +6,45 @@ int main()
     scanf("%d",&mes);
 
     switch (mes)
-}
+{
  case 1: printf ("o %dº mes do calendario e janeiro",mes);
  break;
+
  case 2: printf ("o %dº mes do calendario e fevereiro",mes);
  break;
+
  case 3: printf ("o %dº mes do calendario e marco",mes);
  break;
+
  case 4: printf ("o %dº mes do calendario e abril",mes);
  break;
+
  case 5: printf ("o %dº mes do calendario e maio",mes);
  break;
+
  case 6: printf ("o %dº mes do calendario e junho",mes);
  break;
+
  case 7: printf ("o %dº mes do calendario e julho",mes);
  break;
+
  case 8: printf ("o %dº mes do calendario e agosto",mes);
  break;
+
  case 9: printf ("o %dº mes do calendario e setembro",mes);
  break;
+
  case 10: printf ("o %dº mes do calendario e outubro",mes);
  break;
  case 11: printf ("o %dº mes do calendario e novembro",mes);
  break;
+
  case 12: printf ("o %dº mes do calendario e dezembro",mes);
  break;
- default: printf ("nao existe um valor correspondente a %s","este valor.");
  
+ default: printf ("nao existe um valor correspondente a %s","este valor.");
+}
+}
 
 
 
