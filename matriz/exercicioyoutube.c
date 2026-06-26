530d474e4ea44db95de434aba03abd3cc9a63da2
dener0105 <razerdenerazer@hotmail.com>
2026-06-26 00:08:47 +0000

    Jun 25, 2026, 9:08 PM

--- matriz/exercicioyoutube.c
+++ matriz/exercicioyoutube.c
@@ -9,27 +9,27 @@ int main()
 }
  case 1: printf ("o %dº mes do calendario e janeiro",mes);
 
- case 2: printf ("o %dº mes do calendario e janeiro",mes);
+ case 2: printf ("o %dº mes do calendario e fevereiro",mes);
 
- case 3: printf ("o %dº mes do calendario e janeiro",mes);
+ case 3: printf ("o %dº mes do calendario e marco",mes);
 
- case 4: printf ("o %dº mes do calendario e janeiro",mes);
+ case 4: printf ("o %dº mes do calendario e abril",mes);
 
- case 5: printf ("o %dº mes do calendario e janeiro",mes);
+ case 5: printf ("o %dº mes do calendario e maio",mes);
 
- case 6: printf ("o %dº mes do calendario e janeiro",mes);
+ case 6: printf ("o %dº mes do calendario e junho",mes);
 
- case 7: printf ("o %dº mes do calendario e janeiro",mes);
+ case 7: printf ("o %dº mes do calendario e julho",mes);
 
- case 8: printf ("o %dº mes do calendario e janeiro",mes);
+ case 8: printf ("o %dº mes do calendario e agosto",mes);
 
- case 9: printf ("o %dº mes do calendario e janeiro",mes);
+ case 9: printf ("o %dº mes do calendario e setembro",mes);
 
- case 10: printf ("o %dº mes do calendario e janeiro",mes);
+ case 10: printf ("o %dº mes do calendario e outubro",mes);
 
- case 11: printf ("o %dº mes do calendario e janeiro",mes);
+ case 11: printf ("o %dº mes do calendario e novembro",mes);
 
- case 12: printf ("o %dº mes do calendario e janeiro",mes);
+ case 12: printf ("o %dº mes do calendario e dezembro",mes);
 
 
 
