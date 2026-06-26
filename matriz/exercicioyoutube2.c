4b9d8d65e92d184e8fd7f0b28883dadea8e752a4
dener0105 <razerdenerazer@hotmail.com>
2026-06-26 00:45:45 +0000

    Jun 25, 2026, 9:45 PM

--- matriz/exercicioyoutube2.c
+++ matriz/exercicioyoutube2.c
@@ -4,16 +4,25 @@ int main(){
 int a,b,c;
 printf("\nentre com o valor de a : ");
 scanf("%d"),&a;
-    switch (a){
+    switch (a)
+    {
         case 1:
         b=2;
         break;
-        
+    {
         case 2:
-        b= a*b;
-        break;
-        {
-        
-        }
+        c=3;
+        b= a*c;
     }
-}
+        break;
+        case 3;
+        c=a;
+        default:
+        b=500;
+    }
+   printf("\n valor de a = %d,       valor de b = %d,     valor de c = %d\n"a,b,c);
+    }
+
+        
+        
+
