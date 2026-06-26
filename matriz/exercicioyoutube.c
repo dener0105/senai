4b40490562a731b97ea34ce31f56d811867395f6
dener0105 <razerdenerazer@hotmail.com>
2026-06-26 00:04:55 +0000

    Jun 25, 2026, 9:04 PM

---
+++ matriz/exercicioyoutube.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int mes;
+    printf("\ndigite um valor de 1 a 12 >: ");
+    scanf("%d",&mes);
+
+    switch (mes)
+}
+case 1: printf ("o %dº mes do calendario e janeiro",mes);
