c04a7e881d1eab7d4e9e82ad59d1bd449aac02e2
dener0105 <razerdenerazer@hotmail.com>
2026-06-26 00:38:51 +0000

    Jun 25, 2026, 9:38 PM

---
+++ matriz/exercicioyoutube2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+int a,b,c;
+printf("\nentre com o valor de a : ");
+scanf("%d"),&a;
+    switch (a){
+        case 1:
+        b=2;
+        break;
+        
+        case 2:
+        b= a*b;
+        break;
+        {
+        
+        }
+    }
+}
